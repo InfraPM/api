671f02d2ca866360c23dd8d4c843f89a676a24de
Emily Gouge <egouge@refractions.net>
2025-10-15 13:08:01 -0700

    remove proxy; use apache proxy instead

--- layerinfo/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require '../../support/pass.php';
-require 'LayerInfoApi.php';
-$rawPost = file_get_contents("php://input");
-$layerInfoApi = new LayerInfoApi();
-$layerInfoApi->apiRequest->setRawPostVar($rawPost);
-$layerInfoApi->apiRequest->setGetVar($_GET);
-$layerInfoApi->processRequest();
-$layerInfoApi->sendResponse();
